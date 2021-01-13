ca974aba67b0de99d70b644b94b0693a9b4c0726
beverleyokogwu <beverley.okogwu@gmail.com>
2021-01-13 10:59:09 -0500

    added reference

--- python_functions.py
+++ python_functions.py
@@ -1,1 +1,6 @@
 print("this is an easy python function that basically displays output to the user...")
+
+'''
+All questions originate from w3resource:
+Source: https://www.w3resource.com/python-exercises/python-functions-exercises.php
+'''
