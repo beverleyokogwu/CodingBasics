a5874d987fa5b168ad8c72113b55adec89e3f424
beverleyokogwu <beverley.okogwu@gmail.com>
2021-01-13 10:20:41 -0500

    Intro packets and navigating atom in 3 different extensions!!!

---
+++ PracticePython.py
@@ -0,0 +1,10 @@
+'''
+"You really don't know how much you know until you try it!"- Me!
+'''
+
+# Print "Hello me!"
+print("Hello, me!")
+
+'''
+If you can do this much, and you understand it, then you're a destined to be a coding expert!
+'''
