00e9555dbdc112334338597be5ec8bc9ad359c26
beverleyokogwu <beverley.okogwu@gmail.com>
2021-01-13 13:00:34 -0500

    tested the function on terminal

--- python_functions.py
+++ python_functions.py
@@ -4,3 +4,16 @@ print("this is an easy python function that basically displays output to the use
 All questions originate from w3resource:
 Source: https://www.w3resource.com/python-exercises/python-functions-exercises.php
 '''
+
+'''
+1. Write a Python function to find the Max of three numbers.
+'''
+# My basic solution
+def maxofthree(x,y,z):
+    if x>y and x>z:
+        return x
+    elif y>z:
+        return y
+    else:
+        return z
+print(maxofthree(1,45,5))
